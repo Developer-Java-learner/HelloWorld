e1111ac7e033edf5fbc09bcdb24cd294e9c7d219
Ashish <Ashish@DESKTOP-36MHEC7>
2019-10-09 18:46:06 +0530

    First Commit

---
+++ Hello world/src/Hello.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+//import java.math.BigDecimal;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int a= scan.nextInt(),b= scan.nextInt();
+		System.out.println(a+b);
+		scan.close();
+		
+		
+		
+		/*System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		*/
+		/*
+		 * System.out.println("Hello World"); int a=2147483647; System.out.println(a);
+		 */
+		/*
+		 * int marker =100; double percentage = marker*0.46f;
+		 * System.out.println("percentage: "+percentage);
+		 */
+		/*
+		 * double x=1.05d; double y = 2.55d; System.out.println(x+y); BigDecimal d1 =
+		 * new BigDecimal(1.05); BigDecimal d2 = new BigDecimal(2.55);
+		 * System.out.println(d1.add(d2)); BigDecimal d3 = new BigDecimal("1.05");
+		 * BigDecimal d4 = new BigDecimal("2.55"); System.out.println(d3.add(d4));
+		 */
+		/*
+		 * int a=25; float b=42.159f; double value = a*a+2*a*b+b*b;
+		 * System.out.println(value);
+		 * 
+		 */
+		/*
+		float a = 2.5f;
+		System.out.println(a%2);
+		*/
+		/*
+		 int a=3;
+		    a=a++;
+		    System.out.println("Post-Increment a: " +a);
+		 
+		    a=++a;
+		    System.out.println("Pre-Increment a: "+a );
+		 
+		    a=a--;
+		    System.out.println("Post-Decrement a: " +a);
+		 
+		    a=--a;
+		    System.out.println("Pre-Decrement a: "+a );
+		*/
+		//A a = new C();
+		//B b = new B();
+		//C c = new C();
+		//a.print();
+	}
+}
